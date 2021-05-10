cb91d112f208a020fc28949736c85e60d15a6d51
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-10 19:32:32 +0200

    Add materials to forms

--- js/Arena.js
+++ js/Arena.js
@@ -4,16 +4,27 @@ Arena = function(game) {
     var scene = game.scene;
 
     // Create the main light of the screen
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene)
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 20, 0), scene)
+
+    // Create a material
+    var materialGround = new BABYLON.StandardMaterial("groundTexture", scene)
+    materialGround.diffuseTexture = new BABYLON.Texture("assets/images/brick.jpg", scene)
+    materialGround.diffuseTexture.uScale = 4.0
+    materialGround.diffuseTexture.vScale = 4.0
+
+    var materialWall = new BABYLON.StandardMaterial("wallTexture", scene)
+    materialWall.diffuseTexture = new BABYLON.Texture("assets/images/wood.jpg", scene)
 
     var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene)
     ground.scaling = new BABYLON.Vector3(2, 10, 3)
     ground.scaling.z = 2
+    ground.material = materialGround
 
     var mainBox = BABYLON.Mesh.CreateBox("box1", 3, scene)
     mainBox.scaling.y = 1
     mainBox.position = new BABYLON.Vector3(5, ((3/2)*mainBox.scaling.y), 5)
     mainBox.rotation.y = (Math.PI*45)/180
+    mainBox.material = materialWall
 
     var mainBox2 = mainBox.clone("box2")
     mainBox2.scaling.y = 2
@@ -29,5 +40,6 @@ Arena = function(game) {
 
     var cylinder = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene)
     cylinder.position.y = 20/2
+    cylinder.material = materialWall
 
 }
